b3754ea8fc52291c765b61bab725bdb2726580bf
Zoz <g.zozine@gmail.com>
2013-07-12 15:41:41 +0200

    Specifications : collect method has been re-written Thanks to Skelz0r's explanations, a new version of the specs is born

--- lib/tweetag/app.rb
+++ lib/tweetag/app.rb
@@ -1,8 +1,20 @@
+require 'twitter'
+
 module Tweetag
   class Collector
 
+    attr_accessor :accounts, :hashtag, :result, :result_acc, :result_tweets
+    def initialize
+      
+      Twitter.configure do |config|
+        config.consumer_key = "KgdRthRd0H1LvTcYMxBxJQ"
+        config.consumer_secret = "dQGaEcbPPIbnM4On6eyHNrYwp1lEPGv8vM3RlxLt8" 
+        config.oauth_token = "90441345-JEcCGq1OnBaAl5sNzdN2diHk1vgtLvWoKDWbXV54e" 
+        config.oauth_token_secret = "YlvawOP9bAI979qBRusPStWHYvg1oQh5tF57sAfHo" 
+      end
+    end
+
     def collect
-    
     end
 
 
--- spec/tweetag/app_spec.rb
+++ spec/tweetag/app_spec.rb
@@ -1,40 +1,41 @@
 require 'spec_helper'
-require 'twitter'
 
-module Tweetag
-  describe Collector do
+describe Tweetag::Collector do
 
-    describe "#collect" do
-      it "collects tweets from the accounts we set with the hashtags we want"
-     
-      tweetos = Array.new
-      hashtag = String.new
-
-      collec = Collector.new(tweetos, hashtag)
-      # un objet de type Collector est créé, il contiendra 
-      # la liste prédéfinie des tweetos dont on veut
-      # récupérer les tweets et le hashtag des tweets
-      # qui nous intéressent
-      resultat = Twitter::SearchResults.new
-      # un objet de type SearchResults est créé
-      # c'est la dedans que seront stockés les 
-      # résultats de notre requete
+  describe "#collect" do
+    
+    it "collects tweets from the accounts we set" do
       
-      resultat.results.map do |status|
-        collec.tweetos.should include?(status.from_user)
-        status.text.should include?(collec.hashtag)
+      retriever = Tweetag::Collector.new 
+      # le compte qu'on veut est prédéfini dans Collector.accounts
+      
+      retriever.collect
+      
+      retriever.result_acc.each do |cur|
+        retriever.accounts.should include(cur)
+      end
+      # on placera dans result_acc les comptes
+      # des tweets qu'on a récupéré
+
+    end
+
+    it "collects tweets with the hashtag we want" do 
+
+      retriever = Tweetag::Collector.new 
+
+      retriever.collect
+
+      retriever.result_tweets.each do |cur|
+        cur.should include(retriever.hashtag)
       end
 
-      # on va vérifier que chacun des résultats est bien
-      # émis par un tweetos de notre liste et que 
-      # le hashtag qui nous intéresse est bien contenu 
-      # dans chacun des tweets récéptionnés
-      
     end
   
-    describe "#display" do
-      it "displays tweets properly"
-    end
-
   end
+
+  describe "#display" do
+    it "displays tweets properly" do
+    end
+  end
+
 end
